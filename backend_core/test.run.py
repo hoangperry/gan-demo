54a1645071bd4d3ee2b328b58761ca6c4a484d9e
hoangperry <prodai9573@gmail.com>
2019-12-01 17:46:14 +0700

    update allowed host

--- backend_core/test.run.py
+++ backend_core/test.run.py
@@ -7,14 +7,17 @@ import cv2
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
 from torchvision.utils import save_image
-from mnist import Generator
-
-gen = Generator()
-gen.load('mnist_g.dg')
-z = Variable(torch.Tensor(np.random.normal(0, 1, (1, 100))))
-img = gen.infer(z)
-
-
-save_image(img.view(1, 28, 28).data[:25], 'd.png',  nrow=5, normalize=True)
+from cifar import Generator
+# from mnist import Generator
+#
+# gen = Generator()
+# gen.load('mnist_g.dg')
+# z = Variable(torch.Tensor(np.random.normal(0, 1, (1, 100))))
+# img = gen.infer(z)
+#
+#
+# save_image(img.view(1, 28, 28).data[:25], 'd.png',  nrow=5, normalize=True)
 # img = img.data.numpy().reshape((28, 28))
 # cv2.imwrite('abc.png', img)
+
+G = Generator().to('cpu')
